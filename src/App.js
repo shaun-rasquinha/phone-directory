467b8caa914de9f58ac3d722cd3b658f56188cb6
Shaun <shaunrasquinha@gmail.com>
2024-06-13 03:11:59 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {Component} from "react";
+import { Fragment } from "react";
 
 
 class App extends Component{
    render() {
     return (
-      <div>
-        Phone Directory
-      </div>
+      <Fragment>
+        <div>
+        <div className="header">
+          Phone Directory
+        </div>
+        <button>
+          Add
+        </button>
+        <div>
+          <span>Name </span>
+          <span>Phone </span>
+        </div>
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" placeholder="Type here" defaultValue={"Shilpa"}></input>
+        </div>
+      </Fragment>
+      
     );
   }
 }
